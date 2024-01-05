8143ce70e476eb03208efcc03ba89309e0b36bb9
MarcoCarvalhoDev <dev.carvalho1990@gmail.com>
2024-01-05 00:45:58 -0300

    refatorando: criando unica funcao que altera o layout da pagina

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
+
 focoBt.addEventListener('click', () => { // adicionar eventos através do método .addEventListener
-  html.setAttribute('data-contexto', 'foco') // adicionar um atributo ao elemento HTML.
-  banner.setAttribute('src', 'imagens/foco.png') 
+  alterarContexto('foco');
 })
 
 curtoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto')
-  banner.setAttribute('src', 'imagens/descanso-curto.png') 
+  alterarContexto('descanso-curto');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', 'imagens/descanso-longo.png') 
+  alterarContexto('descanso-longo'); 
   })
 
-  
+  function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto); // adicionar um atributo ao elemento HTML.
+    banner.setAttribute('src', `imagens/${contexto}.png`); // atenção para usar crase ` ` para usar template string ${}
+  }
