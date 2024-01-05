3489cf84b1c12fa423d8d18558f38d045e12604d
MarcoCarvalhoDev <dev.carvalho1990@gmail.com>
2024-01-05 00:56:53 -0300

    alterando texto da pag com innerHTML ...

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
-
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => { // adicionar eventos através do método .addEventListener
   alterarContexto('foco');
@@ -20,4 +20,23 @@ longoBt.addEventListener('click', () => {
   function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto); // adicionar um atributo ao elemento HTML.
     banner.setAttribute('src', `imagens/${contexto}.png`); // atenção para usar crase ` ` para usar template string ${}
+    switch (contexto) { // alterar o texto da página usando o método variavel.innerHTML
+      case "foco":
+          titulo.innerHTML =  `  
+          Otimize sua produtividade,<br>
+              <strong class="app__title-strong">mergulhe no que importa.</strong>
+          `
+          break; // break serve para encerrar a leitura deste bloco e continua executando o código depois do laço
+      case "descanso-curto":
+          titulo.innerHTML = `
+          Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+          ` 
+          break;
+      case "descanso-longo":
+          titulo.innerHTML = `
+          Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+          `
+      default:
+          break;
+  }
   }
